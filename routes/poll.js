ece595d1d4ed9f5ed2d93fdc179b2bccfc2711b0
uta200105 <uta200105@yahoo.com>
2018-12-04 10:12:20 -0600

    initial setup

--- routes/poll.js
+++ routes/poll.js
@@ -3,13 +3,7 @@ const router = express.Router();
 
 const Pusher = require('pusher');
 
-var pusher = new Pusher({
-  appId: '663943',
-  key: '07b2349709a62a89095b',
-  secret: 'd56e747592f57bf369ca',
-  cluster: 'us2',
-  encrypted: true
-});
+
 
 router.get('/', (req, res) => {
     res.send('POLL');
